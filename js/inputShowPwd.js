474faa620f02b35729c4c39b5d991140011a4047
xuyongsky123 <xuyongsky123@outlook.com>
2017-01-11 15:15:09 +0800

    input框密码的明文显示功能封装js组件

--- js/inputShowPwd.js
+++ js/inputShowPwd.js
@@ -1,48 +1,83 @@
 function inputShowPwd(ele){
-    var T = this;
-    this.container = $('.'+ele);  //input包装容器
-    this.inputEle = this.container.find('input[type="password"]');   //input输入框
-    this.deleteEle = this.inputEle.next(); //清空按钮
+
+    this.createNewInput = function(oldObject, oType) {
+
+        var newObject = document.createElement('input');
+
+        newObject.type = oType;
+        if(oldObject.size) newObject.size = oldObject.size;
+        if(oldObject.value) newObject.value = oldObject.value;
+        if(oldObject.name) newObject.name = oldObject.name;
+        if(oldObject.className) newObject.className = oldObject.className;
+
+        return newObject;
+
+    }
 
     //初始化样式及效果
-    this.init();
+    this.init(ele);
+
+    return document.getElementsByClassName(ele);
+
 }
 
-inputShowPwd.prototype.init = function(){
+inputShowPwd.prototype.init = function(ele){
+
     var T = this;
+    this.container = $('.'+ele);  //input包装容器
+    this.inputEle = this.container.children('input[type="password"]');   //input输入框
+    this.deleteEle = this.container.children('.showEle'); //清空按钮
 
     (function(){
         //处理ie9不支持栅格键点击触发onpropertychange事件
         if(navigator.appName == 'Microsoft Internet Explorer'){
             if(navigator.appVersion.match(/9./i) == '9.'){
                 T.inputEle.keyup(function(event){
+
+                    var t = $(this);
+                    var _parent = t.parent();
+                    var _btn = _parent.children('.showEle');
                     if(event.keyCode == 8){
-                        ($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
-                        $(this).prev().val($(this).val());
+                        ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
                     }
+
                 });
             }
         }
 
         T.inputEle.on('focus',function(){
-            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100); 
-            $(this).prev().val($(this).val());
+
+            var t = $(this);
+            var _btn = t.parent().children('.showEle');
+
+            ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
 
         }).on('input propertychange',function(){
-            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
-            $(this).prev().val($(this).val());
+
+            var t = $(this);
+            var _btn = t.parent().children('.showEle');
+
+            ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
+
         });
 
         T.deleteEle.on('mousedown',function(){
-            $(this).prev().hide();
-            $(this).parent().children('input[type="text"]').show();
-        }).on('mouseup mouseout',function(){
-            $(this).parent().children('input[type="text"]').hide();
-            $(this).prev().show();
-        });
 
+            var _parent = $(this).parent();
+            var _pwd = _parent.children('input[type="password"]');
+
+            _pwd.hide();
+            _parent.append($(T.createNewInput(_pwd.get(0),'text')).show());
+
+        }).on('mouseup mouseout',function(){
+
+            var _parent = $(this).parent();
+
+            _parent.find('input[type="text"]').remove();
+            _parent.children('input[type="password"]').show();
+
+        });
 
     })();
 
-    
 }
---
+++ js/inputShowPwd的副本.js
@@ -0,0 +1,48 @@
+function inputShowPwd(ele){
+    var T = this;
+    this.container = $('.'+ele);  //input包装容器
+    this.inputEle = this.container.find('input[type="password"]');   //input输入框
+    this.deleteEle = this.inputEle.next(); //清空按钮
+
+    //初始化样式及效果
+    this.init();
+}
+
+inputShowPwd.prototype.init = function(){
+    var T = this;
+
+    (function(){
+        //处理ie9不支持栅格键点击触发onpropertychange事件
+        if(navigator.appName == 'Microsoft Internet Explorer'){
+            if(navigator.appVersion.match(/9./i) == '9.'){
+                T.inputEle.keyup(function(event){
+                    if(event.keyCode == 8){
+                        ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
+                        $(this).prev().val($(this).val());
+                    }
+                });
+            }
+        }
+
+        T.inputEle.on('focus',function(){
+            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100); 
+            $(this).prev().val($(this).val());
+
+        }).on('input propertychange',function(){
+            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
+            $(this).prev().val($(this).val());
+        });
+
+        T.deleteEle.on('mousedown',function(){
+            $(this).prev().hide();
+            $(this).parent().children('input[type="text"]').show();
+        }).on('mouseup mouseout',function(){
+            $(this).parent().children('input[type="text"]').hide();
+            $(this).prev().show();
+        });
+
+
+    })();
+
+    
+}
